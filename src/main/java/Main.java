c09f8adba68d63d689a7e52533e9f98629a7416d
Audrey <ay.dubois@gmail.com>
2021-03-02 14:50:49 +0100

    [ADD] ParserXml / config.xml / class Message / class AssocPatternResponse / class ChatBotData

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import util.ChatBotData;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ChatBotData.getInstance().getPatternResponse().forEach(pr -> {
+            pr.afficher();
+            System.out.println("//////////////////");
+        });
+    }
+}
---
+++ src/main/java/util/ChatBotData.java
@@ -0,0 +1,31 @@
+package util;
+
+import bot.AssocPatternResponse;
+
+import java.util.ArrayList;
+
+public class ChatBotData {
+    private static volatile ChatBotData instance = null;
+    private ArrayList<AssocPatternResponse> patternResponse = new ArrayList<>();
+
+
+    private ChatBotData() {
+        ParseXml parseXml = new ParseXml.ParseBuilder("fichiers/bot/config.xml").build();
+        patternResponse.addAll(parseXml.getPatternResponse());
+    }
+
+    public static ChatBotData getInstance() {
+        if (instance == null) {
+            synchronized (ChatBotData.class) {
+                instance = new ChatBotData();
+            }
+        }
+        return instance;
+    }
+
+
+    public ArrayList<AssocPatternResponse> getPatternResponse() {
+        return patternResponse;
+    }
+
+}
